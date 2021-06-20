5f4d4b2814fb190c35926ebadc345d22798d37c1
Kpokc <pavmakarovs@gmail.com>
2021-06-20 17:35:18 +0100

    JQ get length of each 'post' content

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.fixed-action-btn').floatingActionButton({direction: 'left'});
+
+
+    // Get length of posts content and hide part of it
+    var card_title = $(".card-title").siblings().children()[0];
+
+    $(card_title).each(function(){
+        $(this).text("aaaaaaa")
+    })
+
+    
+
 });
+
