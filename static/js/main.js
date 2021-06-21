f6879dddd8c0815b1d3c2de2a11a23a3df84a331
Kpokc <pavmakarovs@gmail.com>
2021-06-21 22:22:22 +0100

    Add for loop into cards.html, aside.html code moved into cards.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.fixed-action-btn').floatingActionButton({direction: 'left'});
-
+    
 
     // Get length of posts content and hide part of it
     var card_title = $(".card-title").siblings().children()[0];
-
     $(card_title).each(function(){
-        $(this).text("aaaaaaa")
+
+        var content = $(this).text().substring(0, 165);
+        var content_length = $(this).text().length;
+
+        if (content_length > 165) {
+            $(this).text(content + "...")
+        }
     })
-
-    
-
 });
 
