a89b49f0524517c529fd9469bc90a45cb2e130f9
Kpokc <pavmakarovs@gmail.com>
2021-06-21 23:06:29 +0100

    Sorting issue with background height using JQ

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.fixed-action-btn').floatingActionButton({direction: 'left'});
+
+    $( window ).resize(function() {
+        var wind_height = $("body").height();
+        console.log(wind_height);
+        $(".area").css("height", wind_height);
+        $(".circles").css("height", wind_height);
+        
+        
+      });
     
 
     // Get length of posts content and hide part of it
-    var card_title = $(".card-title").siblings().children()[0];
+    var card_title = $(".card-title").siblings().children("p");
+    console.log(card_title);
     $(card_title).each(function(){
 
         var content = $(this).text().substring(0, 165);
         var content_length = $(this).text().length;
+
+        console.log(content_length);
 
         if (content_length > 165) {
             $(this).text(content + "...")
