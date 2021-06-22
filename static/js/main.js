036fba6ffcdbd4e8aaf207ebb1dc685d771152f4
Kpokc <pavmakarovs@gmail.com>
2021-06-22 20:36:45 +0100

    Add profile template

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.fixed-action-btn').floatingActionButton({direction: 'left'});
-
-    $( window ).resize(function() {
-        var wind_height = $("body").height();
-        console.log(wind_height);
-        $(".area").css("height", wind_height);
-        $(".circles").css("height", wind_height);
-        
-        
-      });
     
-
     // Get length of posts content and hide part of it
     var card_title = $(".card-title").siblings().children("p");
     console.log(card_title);
