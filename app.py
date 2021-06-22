c095b322f47e82e37c27faeb3af3aeeb44e2d1bf
Kpokc <pavmakarovs@gmail.com>
2021-06-22 21:58:16 +0100

    Add login.html

--- app.py
+++ app.py
@@ -37,6 +37,11 @@ def add_post():
     return render_template("add_post.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
