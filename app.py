401fd4db5684a2e39f97a922e7a9cd8cc2d599ad
Kpokc <pavmakarovs@gmail.com>
2021-06-22 22:11:44 +0100

    SignIn changed to SignUp

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/signin")
-def signin():
-    return render_template("signin.html")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
